e0797ad666011f9617dd4712de801d75eef07aa0
PRANTA Dutta <prantadutta1997@gmail.com>
2020-11-22 12:18:14 +0600

    updated-prisma-env-to-deploy

--- src/components/dashboard/MainContent.tsx
+++ src/components/dashboard/MainContent.tsx
@@ -6,6 +6,11 @@ const MainContent: React.FC<MainContentProps> = ({}) => {
   return (
     <div className="col-span-4">
       <MainContentNav />
+      <div className="grid grid-cols-6 gap-4">
+        <div className="col-start-2 col-span-4">
+          <h2>Hello World</h2>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/dashboard/MainContentNav.tsx
+++ src/components/dashboard/MainContentNav.tsx
@@ -2,8 +2,8 @@ interface MainContentNavProps {}
 
 const MainContentNav: React.FC<MainContentNavProps> = ({}) => {
   return (
-    <div className="flex justify-end items-center bg-gray-200 p-4">
-      <div className="flex items-center mr-4">
+    <div className="flex justify-end items-center bg-gray-200 pr-4">
+      <div className="flex items-center cursor-pointer p-4">
         <div>
           <svg
             className="w-6 h-6 mx-2"
@@ -13,9 +13,9 @@ const MainContentNav: React.FC<MainContentNavProps> = ({}) => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
             ></path>
           </svg>
@@ -25,7 +25,7 @@ const MainContentNav: React.FC<MainContentNavProps> = ({}) => {
           <h4 className="font-semibold">Tk. 2000</h4>
         </div>
       </div>
-      <div className="flex items-center mr-4">
+      <div className="flex items-center cursor-pointer  p-4 hover:bg-gray-300 rounded">
         <div>
           <svg
             className="w-6 h-6 mx-2"
@@ -35,9 +35,9 @@ const MainContentNav: React.FC<MainContentNavProps> = ({}) => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
             ></path>
           </svg>
@@ -47,8 +47,8 @@ const MainContentNav: React.FC<MainContentNavProps> = ({}) => {
           <h4 className="font-semibold">Pranta Dutta</h4>
         </div>
       </div>
-      <div>
-        <h4 className="p-2 cursor-pointer rounded-full border-solid border-2 border-purple-800 hover:bg-purple-900">
+      <div className="p-4">
+        <h4 className="p-2 cursor-pointer rounded border-solid border-2 border-purple-800 hover:bg-purple-900 hover:text-white">
           Log Out
         </h4>
       </div>
