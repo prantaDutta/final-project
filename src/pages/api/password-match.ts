8b8779e09eee7597d5c403e628e48eee512a9d31
PRANTA Dutta <=>
2020-12-05 19:54:40 +0600

    updated-api

--- src/pages/api/password-match.ts
+++ src/pages/api/password-match.ts
@@ -3,30 +3,30 @@ import bcrypt from "bcrypt";
 import prisma from "../../lib/prisma";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  return new Promise(async (resolve, reject) => {
-    if (req.body.password) {
-      const { email, password } = req.body;
-      // console.log(req.body);
-      const user = await prisma.users.findUnique({
-        where: {
-          email,
-        },
-      });
+  // return new Promise(async (resolve, reject) => {
+  if (req.body.password) {
+    const { email, password } = req.body;
+    // console.log(req.body);
+    const user = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
 
-      if (user && (await bcrypt.compare(password, user.password))) {
-        // password match
-        res.json({ msg: "Password Matching" });
-        resolve();
-      } else {
-        // password did not match
-        res.json({
-          msg: "Wrong Credentials",
-        });
-        resolve();
-      }
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // password match
+      res.json({ msg: "Password Matching" });
+      // resolve();
     } else {
-      res.send("Validating");
-      reject();
+      // password did not match
+      res.json({
+        msg: "Wrong Credentials",
+      });
+      // resolve();
     }
-  });
+  } else {
+    res.send("Validating");
+    // reject();
+  }
+  // });
 };
--- src/pages/api/unique-email.ts
+++ src/pages/api/unique-email.ts
@@ -2,36 +2,36 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  return new Promise(async (resolve, reject) => {
-    // console.log(req.body);
-    if (req.body.email) {
-      const { email } = req.body;
-      //   res.json({
-      //     email,
-      //   });
-      try {
-        const user = await prisma.users.findUnique({
-          where: {
-            email,
-          },
+  // return new Promise(async (resolve, reject) => {
+  // console.log(req.body);
+  if (req.body.email) {
+    const { email } = req.body;
+    //   res.json({
+    //     email,
+    //   });
+    try {
+      const user = await prisma.users.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (user) {
+        res.json({
+          msg: "Email already been taken",
         });
-        if (user) {
-          res.json({
-            msg: "Email already been taken",
-          });
-          resolve();
-        } else {
-          res.json({ msg: "Unique Email" });
-          resolve();
-        }
-      } catch (e) {
-        console.log(e);
-        res.send("Validating");
-        reject();
+        // resolve();
+      } else {
+        res.json({ msg: "Unique Email" });
+        // resolve();
       }
-    } else {
+    } catch (e) {
+      console.log(e);
       res.send("Validating");
-      reject();
+      // reject();
     }
-  });
+  } else {
+    res.send("Validating");
+    // reject();
+  }
+  // });
 };
