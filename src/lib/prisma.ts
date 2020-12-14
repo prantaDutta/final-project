0d3cf2607c6a161777217052a579f37278bddda7
PRANTA Dutta <prantadutta1997@gmail.com>
2020-12-15 00:59:02 +0600

    updated-prisma-ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { isProduction } from "../utils/constants";
 
-let prisma;
-const globalAny: any = global;
-
-if (isProduction) {
-  prisma = new PrismaClient();
-} else {
-  if (!globalAny.prisma) {
-    globalAny.prisma = new PrismaClient();
-  }
-
-  prisma = globalAny.prisma;
-}
-
+const prisma = new PrismaClient();
 export default prisma;
